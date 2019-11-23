5d41e3092ba3070627e984b743ed6bc5e7273a12
Gerald Todd <beingforthebenefit@gmail.com>
2019-11-23 09:59:58 -0800

    play.py now plays the opening line of Fur Elise with sine waves.

--- play.py
+++ play.py
@@ -3,23 +3,31 @@
 import pyaudio
 import numpy
 
-p = pyaudio.PyAudio()
+def noteToFrequency(note):
+        return 27.5 * ((2.0 ** (1.0/12.0)) ** float(note-1))
 
-volume = 0.5
-fs = 44100
-duration = 1.0
-f = 440.0
+def playNote(note, duration):
+    p = pyaudio.PyAudio()
+    volume = 0.5
+    sampleRate = 44100
+    frequency = noteToFrequency(note)
+    print(frequency)
+    samples = (numpy.sin(2*numpy.pi*numpy.arange(sampleRate*duration)*frequency/sampleRate)).astype(numpy.float32)
+    stream = p.open(format=pyaudio.paFloat32,
+                    channels=1,
+                    rate=sampleRate,
+                    output=True)
+    stream.write(volume*samples)
+    stream.stop_stream()
+    stream.close()
+    p.terminate()
 
-samples = (numpy.sin(2*numpy.pi*numpy.arange(fs*duration)*f/fs)).astype(numpy.float32)
-
-stream = p.open(format=pyaudio.paFloat32,
-                channels=1,
-                rate=fs,
-                output=True)
-
-stream.write(volume*samples)
-
-stream.stop_stream()
-stream.close()
-
-p.terminate()
+playNote(56, 1)
+playNote(55, 1)
+playNote(56, 1)
+playNote(55, 1)
+playNote(56, 1)
+playNote(51, 1)
+playNote(54, 1)
+playNote(52, 1)
+playNote(49, 3)
